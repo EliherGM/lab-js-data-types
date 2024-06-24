ee78121f3097b84d1eeed0c835266a6604a3a9af
EliherGM <eliher.giorgana.morales@outlook.com>
2024-06-24 19:38:41 +0000

    done iteration1

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5}`;
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -22,10 +22,13 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+const lastLetter = part1[part1.length - 1].toUpperCase();
+const lastLetter2 = part2[part2.length - 1].toUpperCase();
 
+const camelTail = `${part1.slice(0, -1)}${lastLetter}${part2.slice(0, -1)}${lastLetter2}`;
 
 // Print the cameLtaiL-formatted string
-
+console.log(camelTail);
 
 
 
